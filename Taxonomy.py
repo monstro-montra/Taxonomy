80758c6b0af0d4000319294d4463034b2770798a
Alex7861 <alex.ruperto@javierlabs.com>
2023-12-11 04:02:13 -0500

    started working on taxonomy

--- Taxonomy.py
+++ Taxonomy.py
@@ -1,3 +1,24 @@
-class Taxonomy{
-    
-}
+class Taxonomy:
+    def __init__(self):
+        self.taxonomy = {
+            "Restaurants" : {
+                "Italian Food" : [
+                    "Olive Garden",
+                    "Carrabas" 
+                ],  
+                "Asian Food" : {
+                    "Chinese Food" : [
+                        "Panda Express",
+                        "Manchu WOK"
+                    ],
+                    "Japanese Food" : [
+                        ""
+                    ]
+                    },
+                    
+                    
+                },
+
+
+            }
+        }
